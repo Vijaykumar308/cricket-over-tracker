1771203665a915fe9afef5b8639da1f183732a80
vijay kumar <308vijaykumar@gmail.com>
2024-06-11 12:16:30 +0530

    over ball changes

--- src/components/ScorePerBall.jsx
+++ src/components/ScorePerBall.jsx
@@ -1,8 +1,8 @@
 function ScorePerBall({currentScore}) {
     function getTotleScore(arr) {
         const inValidBalls = ['1NB', '1WD'];
-        
-        const totleRuns = arr.reduce((acc, elem) => {
+
+        const totalRuns = arr.reduce((acc, elem) => {
             if (inValidBalls.includes(elem)) {
                 return acc + 1;
             }
@@ -12,13 +12,18 @@ function ScorePerBall({currentScore}) {
 
         }, 0);
 
-        return totleRuns;
+        return totalRuns;
+    }
+
+    function countBalls(arr) {
+        return arr.filter(item => typeof item === 'number').length;
     }
 
     return (
         <div>
             <div className="display-balls-container">
-                <div className="score">{getTotleScore(currentScore)} / 0  <span className="overs">0.2 Overs</span></div>
+                {/* {console.log(countNumbers(currentScore))} */}
+                <div className="score">{getTotleScore(currentScore)} / 0  <span className="overs">{countBalls(currentScore) != 6 ? `0. ${countBalls(currentScore)} Overs` :  "1 Overs" }</span></div>
                 <div className="over-container">
                     <div className="balls">
                         {
